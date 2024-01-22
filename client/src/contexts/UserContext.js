2435da2f26f4b409d5c63906ea0e92be6a00f5ff
philipdemand <117778804+philipdemand@users.noreply.github.com>
2024-01-22 14:26:49 -0500

    auth context

--- client/src/contexts/UserContext.js
+++ client/src/contexts/UserContext.js
@@ -7,17 +7,44 @@ function UserProvider({ children }) {
   const [user, setUser] = useState(null);
   const navigate = useNavigate();
 
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const res = await fetch("/api/v1/me");
+  //       if (res.ok) {
+  //         const data = await res.json();
+  //         setUser(data);
+  //         if (data && data.role_type === "Musician") {
+  //           navigate("/musician");
+  //         } else {
+  //           navigate("/director");
+  //         }
+  //       } else {
+  //         setUser(null);
+  //         navigate("/");
+  //       }
+  //     } catch (error) {
+  //       console.error("An error occurred while fetching user data:", error.message);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const res = await fetch("/api/v1/me");
         if (res.ok) {
           const data = await res.json();
-          setUser(data);
-          if (data && data.role_type === "Musician") {
-            navigate("/musician");
+          if (data) {
+            setUser(data);
+            if (data && data.role_type === "Musician") {
+              navigate("/musician");
+            } else {
+              navigate("/director");
+            }
           } else {
-            navigate("/director");
+            navigate("/");
           }
         } else {
           setUser(null);
