a3bdba139a609cee5113352919e1396a7d1e3a09
philipdemand <117778804+philipdemand@users.noreply.github.com>
2024-01-24 16:02:47 -0500

    no seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
-    # validates :role_type, presence: true
+    validates :role_type, presence: true
     delegated_type :role, types: %w[Director Musician], dependent: :destroy
 end
